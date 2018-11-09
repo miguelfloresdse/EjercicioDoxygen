2250a1b304a7629966f7a5029b7650a7f359f698
Miguel Flores <jose.flores.dse@uteq.edu.mx>
2018-11-09 16:26:06 -0600

    Added main project file

--- addition.cpp
+++ addition.cpp
@@ -1,13 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+using namespace std;
+
 void addition(char selection) {
 	if(selection == 'A') {
+		string initialValue;
 		int x, y, result;
-		cout << "Insert the number: \nValue: ";
-		scanf("%d", x);
+		int outCycle = 0;
+		
+		while(outCycle == 0) {
+			cout << "Insert the number: \nValue: ";
+			scanf("%s", initialValue);
+			
+			for (int i = 0; i < initialValue.size(); i++){
+				if(initialValue[i] == '.') {
+					cout << "The inserted value is not valid, please, introduce valid data.\n";
+					break;
+				} else {
+					outCycle = 1;
+				}
+			}
+		}
+		
+		x = stoi(initialValue);
+		
+		while(outCycle == 0) {
+			cout << "Insert the number: \nValue: ";
+			scanf("%s", initialValue);
+			
+			for (int i = 0; i < initialValue.size(); i++){
+				if(initialValue[i] == '.') {
+					cout << "The inserted value is not valid, please, introduce valid data.\n";
+					break;
+				} else {
+					outCycle = 1;
+				}
+			}
+		}
+		
+		y = stoi(initialValue);
+		
+		cout << "The result for: " << x << " + " << y << " is " << x + y;
+		
+		
 	} else if(selection == 'B') {
 		
 	}
-	cout << "The result for: " << x << " + " << y << " is " << x + y;
 }
